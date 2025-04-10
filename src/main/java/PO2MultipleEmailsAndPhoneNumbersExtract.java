ac3e9e4eaafdf5b4668204f9e876173ed2df2bbb
Alexandar Georgiev <georgievalex807@gmail.com>
2025-04-10 23:01:17 +0300

    feat/Push-the-first-Two-files

--- src/main/java/PO2MultipleEmailsAndPhoneNumbersExtract.java
+++ src/main/java/PO2MultipleEmailsAndPhoneNumbersExtract.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PO2MultipleEmailsAndPhoneNumbersExtract {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         String text=scanner.nextLine();
+         phoneAndEmailChecker(text);
+
+    }
+    private static void phoneAndEmailChecker(String text){
+        Map<String, List<String>> emails=new LinkedHashMap<>();
+        Map<String, List<String>> phones=new LinkedHashMap<>();
+
+        Pattern emailPatter=Pattern.compile("[A-Za-z0-9]+[\\_\\.]*[A-Za-z0-9]*\\@[a-z]+\\.[a-z]+\\.*[a-z]*");
+        Matcher emailMatcher= emailPatter.matcher(text);
+
+
+        Pattern phonePatter=Pattern.compile("\\+\\d{1,3}([\\s|\\-])\\d{2,3}\\1\\d{3,4}\\1\\d{3,4}");
+        Matcher phoneMatcher= phonePatter.matcher(text);
+       emails.put(text,new ArrayList<>());
+       phones.put(text,new ArrayList<>());
+       while (emailMatcher.find()){
+             emails.get(text).add(emailMatcher.group());
+        }
+        while (phoneMatcher.find()){
+            phones.get(text).add(phoneMatcher.group());
+        }
+        System.out.println("Emails: "+ String.join(", ",emails.get(text)));
+        System.out.println("Phone Numbers: "+ String.join(", ",phones.get(text)));
+   }
 }
--- src/main/java/org/example/PO1EmailExtract.java
+++ src/main/java/org/example/PO1EmailExtract.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PO1EmailExtract {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         String text=scanner.nextLine();
+        System.out.println(findEmail(text));
+
+    }
+    public static String findEmail(String text){
+        Pattern pattern =Pattern.compile("[A-Za-z0-9]+[\\_\\.]*[A-Za-z0-9]*\\@[a-z]+\\.[a-z]+\\.*[a-z]*");
+        Matcher matcher= pattern.matcher(text);
+      if (matcher.find()){
+          return  "The email is: "+matcher.group();
+      }
+
+
+        return "This text does not contain email";
+    }
 }
